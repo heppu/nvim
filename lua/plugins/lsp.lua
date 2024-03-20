ba9561143a2f8411de7f2f4c678376f20a50e245
Henri Koski <henri.t.koski@gmail.com>
2024-03-20 12:30:01 +0200

    Add lsp

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,43 @@
+return {
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
+	{'neovim/nvim-lspconfig'},
+	{
+		'williamboman/mason.nvim',
+		config = function()
+			require('mason').setup({})
+		end
+	},
+	{
+		'williamboman/mason-lspconfig.nvim',
+		config = function()
+			require('mason-lspconfig').setup({
+				ensure_installed = {
+					"bashls",
+					"lua_ls",
+					"rust_analyzer",
+					"gopls",
+					"html",
+					"htmx",
+					"jsonls",
+					"tsserver",
+				},
+				handlers = {
+					require('lsp-zero').default_setup,
+				},
+			})
+		end
+	},
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		config = function()
+			local lsp_zero = require('lsp-zero')
+			lsp_zero.extend_lspconfig()
+			lsp_zero.on_attach(function(client, bufnr)
+				lsp_zero.default_keymaps({buffer = bufnr})
+			end)
+		end
+	},
+}
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,6 +6,6 @@ return {
 		"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
 	config = function()
-		vim.keymap.set('n', '<C-n>', ":Neotree<CR>")
+		vim.keymap.set('n', '<leader>n', ":Neotree<CR>")
 	end
 }
