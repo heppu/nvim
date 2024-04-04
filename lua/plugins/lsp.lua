39b7c7ada4854e762c260083e397135a34a63de1
Henri Koski <henri.t.koski@gmail.com>
2024-04-04 12:49:03 +0300

    Add go support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -48,6 +48,7 @@ return {
 			lsp_zero.extend_lspconfig()
 			lsp_zero.on_attach(function(client, bufnr)
 				lsp_zero.default_keymaps({ buffer = bufnr })
+				lsp_zero.buffer_autoformat()
 			end)
 		end,
 	},
